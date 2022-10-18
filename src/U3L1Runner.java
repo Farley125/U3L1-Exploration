b7ccd8d135eaa6b51e4cafa59af07f6eb5c0826c
unknown <BT_4N2_03@BKTECH.LOCAL>
2022-10-18 11:40:05 -0400

    Initial commit

---
+++ src/U3L1Runner.java
@@ -0,0 +1,19 @@
+public class U3L1Runner {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 15;
+        int c = 20;
+        System.out.println("-- Set A --");
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+        System.out.println("-- Set B --");
+        System.out.println(a > c);
+        System.out.println(a < c);
+        System.out.println(a >= c);
+        System.out.println(a <= c);
+
+    }
+}
